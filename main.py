3b9d117104f5aaf3eeead7f76dfd63165d4e6567
Anmol <mailanmolat@gmail.com>
2026-08-14 17:43:12 +0530

    Added Pipeline
    Created a pipeline which will run all the agents step by step

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-# from src.tools.tools import web_search,scrape_url
+from src.pipelines.pipeline import run_research_pipeline
 
-# output = web_search.invoke({
-#     "query": "What is the latest research on AGI?"
-# })
 
-# results = scrape_url.invoke({
-#     "url": "https://www.reddit.com/r/aritificial/"
-# })
-
-# print(results)
+topic = "The impact of AI on the job market in 2026"
+run_research_pipeline(topic)
