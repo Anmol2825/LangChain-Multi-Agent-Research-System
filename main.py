55a530459b601f657025f47a0e62cde445664424
Anmol <mailanmolat@gmail.com>
2026-08-14 17:22:36 +0530

    tools created
    2 tools created one is the tavily for search and second is the beautifulSoup for Scraping

---
+++ main.py
@@ -0,0 +1,11 @@
+# from src.tools.tools import web_search,scrape_url
+
+# output = web_search.invoke({
+#     "query": "What is the latest research on AGI?"
+# })
+
+# results = scrape_url.invoke({
+#     "url": "https://www.reddit.com/r/aritificial/"
+# })
+
+# print(results)
